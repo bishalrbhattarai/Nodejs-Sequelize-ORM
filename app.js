ca4cc3ce84c4dae3c96123741f82d7e210c374e1
Bishal Raj Bhattarai <bishalrajbhattarai01@gmail.com>
2024-11-23 15:01:32 +0545

    user with association implementation

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const { sequelize, User } = require("./models");
+const { sequelize, User, Post } = require("./models");
 const express = require("express");
 const app = express();
 
 app.use(express.json());
 
 function errorHandler(res, error) {
+  console.log(error);
   res.status(500).json({
     success: false,
     error,
@@ -18,15 +19,39 @@ app.get("/user/:id", async (req, res) => {
     id = Number(id);
   }
   try {
-    const foundUser = await User.find({
-      where: {
-        id,
-      },
-    });
+    const foundUser = await User.findByPk(id);
 
     res.status(200).json({
       success: true,
       foundUser,
+    });
+  } catch (error) {
+    errorHandler(res, error);
+  }
+});
+
+app.post("/post/:id", async (req, res) => {
+  let { id } = req.params;
+  const { content } = req.body;
+  if (!id) throw new Error("id not found");
+  if (typeof id === "string") {
+    id = Number(id);
+  }
+  try {
+    const foundUser = await User.findByPk(id);
+    if (!foundUser)
+      return res.status(400).json({
+        success: false,
+        message: "Such User Doesnot Exist.",
+      });
+    const createdPost = await Post.create({
+      content,
+      userId: foundUser.id,
+    });
+
+    res.status(200).json({
+      success: true,
+      createdPost,
     });
   } catch (error) {
     errorHandler(res, error);
